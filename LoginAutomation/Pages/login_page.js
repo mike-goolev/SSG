a2c7b9627156bddeda9f3d83a83beb24fbba7c3f
mike-goolev <goolevm@gmail.com>
2024-03-20 23:37:54 -0500

    added locators and basic functions for tests

--- LoginAutomation/Pages/login_page.js
+++ LoginAutomation/Pages/login_page.js
@@ -1,5 +1,23 @@
 export class LoginPage {
     constructor(page) {
         this.page = page;
+        this.usernameInputField = page.locator('input[id="username"]');
+        this.passwordInputField = page.locator('input[id="password"]');
+        this.loginButton = page.locator('button[type="submit"]');
+    }
+
+    async isUsernameInputFieldVisible() {
+        const usernameInputField = this.usernameInputField;
+        return await usernameInputField.isVisible();
+    }
+
+    async isPasswordInputFieldVisible() {
+        const passwordInputField = this.passwordInputField;
+        return await passwordInputField.isVisible();
+    }
+
+    async isLoginButtonVisible() {
+        const loginButton = this.loginButton;
+        return await loginButton.isVisible();
     }
 }
