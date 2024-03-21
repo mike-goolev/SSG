527ee04a1e382e1aefb7e1bdc62b05ed421bd3f1
mike-goolev <goolevm@gmail.com>
2024-03-20 23:39:07 -0500

    before each method to open login page

--- LoginAutomation/Tests/tests.spec.js
+++ LoginAutomation/Tests/tests.spec.js
@@ -1,4 +1,7 @@
  //test.spec.js
  import {test, expect} from '@playwright/test';
  import { LoginPage } from '../Pages/login-page';
- 
+
+ test.beforeEach(async ({page}) => {
+    await page.goto('https://practice.expandtesting.com/login');
+})
