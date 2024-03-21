c79d35d8ddf54257ea98da7cafe3e4610847ee28
mike-goolev <goolevm@gmail.com>
2024-03-21 00:38:03 -0500

    added functions for login page

--- LoginAutomation/Pages/login_page.js
+++ LoginAutomation/Pages/login_page.js
@@ -6,6 +6,7 @@ export class LoginPage {
         this.submitButton = page.locator('button[type="submit"]');
     }
 
+    // Functions to check the visibility of an element
     async isUsernameInputFieldVisible() {
         const usernameInputField = this.usernameInputField;
         return await usernameInputField.isVisible();
@@ -19,5 +20,28 @@ export class LoginPage {
     async isSubmitButtonVisible() {
         const submitButton = this.submitButton;
         return await submitButton.isVisible();
+    }
+
+    // methods for filling the Username and Password input fields
+    async fillUsername(username) {
+        const usernameInputField = this.usernameInputField;
+        await usernameInputField.fill(username);
+    }
+
+    async fillPassword(password) {
+        const passwordInputField = this.passwordInputField;
+        await passwordInputField.fill(password);
+    }
+
+    // Method to call the fillUsername and fillPassword methods
+    async logIn(username, password) {
+        await this.fillUsername(username);
+        await this.fillPassword(password);
+    }
+
+    // Click on Submit button
+    async clickSubmitLoginButton() {
+        const submitButton = this.submitButton;
+        await submitButton.click();
     }
 }
